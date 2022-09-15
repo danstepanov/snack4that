b0348315f5fc07e5c374a51dcc8bbb3104b3ca50
danstepanov <danhstepanov@gmail.com>
2022-09-15 19:15:05 -0400

    remove nested anchor tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -13,42 +14,43 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <p className={styles.description}>
-          Have a Snack to contribute? <a className={styles.link} href="https://twitter.com/danstepanov.com">Hit me up</a>.
+          Have a Snack to contribute? <a href="https://twitter.com/danstepanov.com">Hit me up</a>.
         </p>
 
         <div className={styles.grid}>
-          <a href="https://snack.expo.dev/@danstepanov/app-update-available-banner&platform=mydevice" className={styles.card}>
-            <h2>App Update Available Banner &rarr;</h2>
-            <p>Show a banner over your app when there is a new over-the-air update available.</p>
-            <a href="https://twitter.com/danstepanov">Created by Dan Stepanov</a>
-          </a>
-          
+          <div className={styles.card}>
+            <a href="https://snack.expo.dev/@danstepanov/app-update-available-banner&platform=mydevice" target="_blank" rel="noopener noreferrer">
+              <h2>App Update Available Banner &rarr;</h2>
+              <p>Show a banner over your app when there is a new over-the-air update available.</p>
+            </a>
+            <a href="https://twitter.com/danstepanov" target="_blank" rel="noopener noreferrer">Created by Dan Stepanov</a>
+          </div>
 
-          <a href="https://snack.expo.dev/@danstepanov/auth-session-with-auth0&platform=mydevice" className={styles.card}>
-            <h2>User Authentication with Expo &rarr;</h2>
-            <p>Set up user authentication using Expo AuthSession and Auth0.</p>
-            <a href="https://twitter.com/danstepanov">Created by Dan Stepanov</a>
-          </a>
+          <div className={styles.card}>
+            <a href="https://snack.expo.dev/@danstepanov/auth-session-with-auth0&platform=mydevice" target="_blank" rel="noopener noreferrer">
+              <h2>User Authentication with Expo &rarr;</h2>
+              <p>Set up user authentication using Expo AuthSession and Auth0.</p>
+            </a>
+            <a href="https://twitter.com/danstepanov" target="_blank" rel="noopener noreferrer">Created by Dan Stepanov</a>
+          </div>
 
-          <a
-            href="https://snack.expo.dev/@bacon/cupertino-nav&platform=mydevice"
-            className={styles.card}
-          >
-            <h2>Cupertino/iOS Nav List &rarr;</h2>
-            <p>Uses all the native platform colors so it can match styles across iOS versions.</p>
-            <a href="https://twitter.com/baconbrix">Created by Evan Bacon</a>
-          </a>
+          <div className={styles.card}>
+            <a href="https://snack.expo.dev/@bacon/cupertino-nav&platform=mydevice" target="_blank" rel="noopener noreferrer">
+              <h2>Cupertino/iOS Nav List &rarr;</h2>
+              <p>Uses all the native platform colors so it can match styles across iOS versions.</p>
+            </a>
+            <a href="https://twitter.com/danstepanov" target="_blank" rel="noopener noreferrer">Created by Dan Stepanov</a>
+          </div>
 
-          <a
-            href="https://snack.expo.dev/@danstepanov/dynamic-button-and-text&platform=mydevice"
-            className={styles.card}
-          >
-            <h2>Dynamic Button and Text &rarr;</h2>
-            <p>
-              Highly reusable Tailwind-style components that allow for theme-based styles.
-            </p>
-            <a href="https://twitter.com/danstepanov">Created by Dan Stepanov</a>
-          </a>
+          <div className={styles.card}>
+            <a href="https://snack.expo.dev/@danstepanov/dynamic-button-and-text&platform=mydevice" target="_blank" rel="noopener noreferrer">
+              <h2>Dynamic Button and Text &rarr;</h2>
+              <p>
+                Highly reusable Tailwind-style components that allow for theme-based styles.
+              </p>
+            </a>
+            <a href="https://twitter.com/danstepanov" target="_blank" rel="noopener noreferrer">Created by Dan Stepanov</a>
+          </div>
         </div>
       </main>
 
