9c20bb93f5e801a2a83a077e6503e9cf45a9b8f9
danstepanov <danhstepanov@gmail.com>
2022-09-17 16:40:50 -0700

    add fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,30 @@
 import '../styles/globals.css'
+import { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import * as Fathom from 'fathom-client'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    Fathom.load('JIAXQCSF', {
+      includedDomains: ['snackforthat.com']
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+  
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
