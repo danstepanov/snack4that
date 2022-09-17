ef52ac944c5bc0e19baec2b9e40727a3e0428c88
danstepanov <danhstepanov@gmail.com>
2022-09-17 16:47:14 -0700

    prevent hover effect on mobile

--- components/SnackCard.tsx
+++ components/SnackCard.tsx
@@ -3,10 +3,10 @@ import Hyperlink from "./Hyperlink"
 
 const SnackCard = ({ key, snack }: { key: Key, snack: SnackCard }) => {
     return (
-        <div className="group flex flex-col border rounded p-2 h-36 justify-between hover:border-blue-500">
+        <div className="group flex flex-col border rounded p-2 h-36 justify-between md:hover:border-blue-500">
             <a href={`${snack.snackUrl}?&platform=mydevice`} target="_blank" rel="noopener noreferrer">
-            <h2 className="font-bold text-slate-900 dark:text-white group-hover:text-blue-500">{`${snack.title} `}&rarr;</h2>
-            <p className="text-sm text-slate-500 dark:text-slate-200 group-hover:text-blue-500 w-4/5 mt-1">{snack.description}</p>
+            <h2 className="font-bold text-slate-900 dark:text-white md:group-hover:text-blue-500">{`${snack.title} `}&rarr;</h2>
+            <p className="text-sm text-slate-500 dark:text-slate-200 md:group-hover:text-blue-500 w-4/5 mt-1">{snack.description}</p>
             </a>
             <Hyperlink baseText="Created by" urlText={snack.author} url={snack.snackUrl} />
         </div>
