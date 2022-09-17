f89c09c7fec17d8e9f8f55089368c26d630a87df
danstepanov <danhstepanov@gmail.com>
2022-09-17 11:50:26 -0700

    fix links

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
 
         <div className={styles.grid}>
           <div className={styles.card}>
-            <a href="https://snack.expo.dev/@danstepanov/app-update-available-banner&platform=mydevice" target="_blank" rel="noopener noreferrer">
+            <a href="https://snack.expo.dev/@danstepanov/app-update-available-banner?&platform=mydevice" target="_blank" rel="noopener noreferrer">
               <h2>App Update Available Banner &rarr;</h2>
               <p>Show a banner over your app when there is a new over-the-air update available.</p>
             </a>
@@ -27,7 +27,7 @@ const Home: NextPage = () => {
           </div>
 
           <div className={styles.card}>
-            <a href="https://snack.expo.dev/@danstepanov/auth-session-with-auth0&platform=mydevice" target="_blank" rel="noopener noreferrer">
+            <a href="https://snack.expo.dev/@danstepanov/auth-session-with-auth0?&platform=mydevice" target="_blank" rel="noopener noreferrer">
               <h2>User Authentication with Expo &rarr;</h2>
               <p>Set up user authentication using Expo AuthSession and Auth0.</p>
             </a>
@@ -35,15 +35,15 @@ const Home: NextPage = () => {
           </div>
 
           <div className={styles.card}>
-            <a href="https://snack.expo.dev/@bacon/cupertino-nav&platform=mydevice" target="_blank" rel="noopener noreferrer">
+            <a href="https://snack.expo.dev/@bacon/cupertino-nav?&platform=mydevice" target="_blank" rel="noopener noreferrer">
               <h2>Cupertino/iOS Nav List &rarr;</h2>
               <p>Uses all the native platform colors so it can match styles across iOS versions.</p>
             </a>
-            <a href="https://twitter.com/danstepanov" target="_blank" rel="noopener noreferrer">Created by Dan Stepanov</a>
+            <a href="https://twitter.com/baconbrix" target="_blank" rel="noopener noreferrer">Created by Evan Bacon</a>
           </div>
 
           <div className={styles.card}>
-            <a href="https://snack.expo.dev/@danstepanov/dynamic-button-and-text&platform=mydevice" target="_blank" rel="noopener noreferrer">
+            <a href="https://snack.expo.dev/@danstepanov/dynamic-button-and-text?&platform=mydevice" target="_blank" rel="noopener noreferrer">
               <h2>Dynamic Button and Text &rarr;</h2>
               <p>
                 Highly reusable Tailwind-style components that allow for theme-based styles.
