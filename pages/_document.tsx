05854dc7be5bcf6a227520017d98b4b53f421ae2
danstepanov <danhstepanov@gmail.com>
2022-09-23 14:28:34 -0700

    add og image and other meta links

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,10 +10,21 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                <meta charSet="utf-8" />
+                    <link rel="icon" href="/favicon.ico" />
                     <meta property="og:title" content="Snack for That" key="ogtitle" />
                     <meta property="og:description" content="A collection of helpful Expo Snacks" key="ogdescription" />
-                    <meta charSet="utf-8" />
-                    <link rel="icon" href="/favicon.ico" />
+                    <meta property="og:image" content="/public/snacks.jpg" key="ogimageurl" />
+                    <meta property="og:image:secure_url" content="/public/snacks.jpg" key="ogimagesecureurl" />
+                    <meta property="og:image:type" content="image/png" key="ogimagetype" />
+                    <meta property="og:image:width" content="1200" key="ogiamgewidth" />
+                    <meta property="og:image:height" content="627" key="ogimageheight" />
+                    <meta property="og:type" content="website" key="ogtype" />
+                    <meta property="og:url" content="snackforthat.com" key="ogurl" />
+                    <meta property="og:title" content="Snack for That" key="ogtitle" />
+                    <meta property="og:description" content="A collection of helpful Expo Snacks" key="ogdescription" />
+                    <meta name="twitter:card" content="summary" key="twcard" />
+                    <meta name="twitter:creator" content="@danstepanov" key="twhandle" />
                 </Head>
                 <body className="bg-white dark:bg-black text-white dark:text-black">
                     <Main />
