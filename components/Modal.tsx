d46d85d90d7d5ac6fe3eb9147e788d00ccc4cc3d
danstepanov <danhstepanov@gmail.com>
2022-09-23 14:07:59 -0700

    fix border on modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -9,9 +9,9 @@ const Modal = ({ toggleModal }: { toggleModal: () => void }) => {
             <div
                 className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
             >
-                <div className="relative my-6 mx-6 md:mx-auto max-w-3xl border-gray-200 dark:border-gray-800 rounded border ">
+                <div className="relative my-6 mx-6 md:mx-auto max-w-3xl ">
                     {/*content*/}
-                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white dark:bg-black outline-none focus:outline-none">
+                    <div className="border border-gray-200 dark:border-white rounded-lg shadow-lg relative flex flex-col w-full bg-white dark:bg-black outline-none focus:outline-none">
                         {/*header*/}
                         <div className="flex items-start justify-between p-5 border-b border-gray-200 dark:border-gray-800 rounded rounded-t">
                             <h3 className="text-3xl font-semibold text-gray-900 dark:text-gray-100">
