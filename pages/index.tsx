33a30494789b1638e3b94416074beb2a92307eb8
danstepanov <danhstepanov@gmail.com>
2022-09-27 11:17:51 -0700

    move data to supabase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import type { NextPage } from "next"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
@@ -10,6 +10,7 @@ import SectionTitle from "../components/SectionTitle"
 import Title from "../components/Title"
 import Modal from "../components/Modal"
 import useModal from "../hooks/useModal"
+import { supabase } from "../utils/supabaseClient"
 
 declare global {
   interface SnackCard {
@@ -22,83 +23,40 @@ declare global {
     snackUrl: string;
   }
 }
-const Home: NextPage = () => {
-  const [snacks] = useState<SnackCard[]>([
-    {
-      id: 0,
-      eventId: "QYQWKZ5X",
-      title: "App Update Available Banner",
-      description: "Optionally present a banner over your app when there is a new over-the-air update available.",
-      twitterUrl: "https://twitter.com/danstepanov",
-      author: "Dan Stepanov",
-      snackUrl: "https://snack.expo.dev/@danstepanov/app-update-available-banner"
-    },
-    {
-      id: 1,
-      eventId: "CQ0VVTQO",
-      title: "User Auth with Expo",
-      description: "As AuthSession uses the auth.expo.io proxy, you'll need to enable Tracking Services, this is not recommended for production.",
-      twitterUrl: "https://twitter.com/danstepanov",
-      author: "Dan Stepanov",
-      snackUrl: "https://snack.expo.dev/@danstepanov/auth-session-with-auth0"
-    },
-    {
-      id: 2,
-      eventId: "4DAIRFIS",
-      title: "Cupertino/iOS Nav List",
-      description: "Visually native iOS list. Uses all the native platform colors so it can match styles across iOS versions.",
-      twitterUrl: "https://twitter.com/baconbrix",
-      author: "Evan Bacon",
-      snackUrl: "https://snack.expo.dev/@bacon/cupertino-nav"
-    },
-    {
-      id: 3,
-      eventId: "EN8VLV0I",
-      title: "Dynamic Button and Text",
-      description: "Highly reusable Tailwind-style components that allow for theme-based styles.",
-      twitterUrl: "https://twitter.com/danstepanov",
-      author: "Dan Stepanov",
-      snackUrl: "https://snack.expo.dev/@danstepanov/dynamic-button-and-text"
-    },
-    {
-      id: 4,
-      eventId: "SZV4VPEB",
-      title: "Tailwind Playground via NativeWind",
-      description: "Play with Tailwind styles in Expo Snack using NativeWind. Do not install the peer dependency when prompted at the bottom of the screen.",
-      twitterUrl: "https://twitter.com/mark__lawlor",
-      author: "Mark Lawlor",
-      snackUrl: "https://snack.expo.dev/@mwlawlor/nativewind"
-    },
-    {
-      id: 5,
-      eventId: "P7UYITDF",
-      title: "Animated Price Ticker",
-      description: "A beautiful price ticker component made with react-native-reanimated. This is just a taste of the beauty of the BeatGig mobile app.",
-      twitterUrl: "https://twitter.com/FernandoTheRojo",
-      author: "Fernando Rojo",
-      snackUrl: "https://snack.expo.dev/@beatgig/reanimated-price-ticker"
-    },
-    {
-      id: 6,
-      eventId: "AAK4EX9G",
-      title: "Text Input above Keyboard",
-      description: "This snack uses a hook to present an input above the keyboard. My only suggestion is to have the keyboard open upon clicking the button.",
-      twitterUrl: "https://twitter.com/nishanbende",
-      author: "Nishan Bende",
-      snackUrl: "https://snack.expo.dev/@nishanbende/keyboard-avoid-hook"
-    },
-    {
-      id: 7,
-      eventId: "CODPGHIZ",
-      title: "Skia FlashList",
-      description: "A demo of the incredibly powerful and quick FlashList, made by Shopify. This snack comes to us from one of the smartest and kindest engineers I've ever worked with.",
-      twitterUrl: "https://twitter.com/cedricvanputten",
-      author: "Cedric Van Putten",
-      snackUrl: "https://snack.expo.dev/@bycedric/skia-flash-list"
-    }
-  ])
-  const sortedSnacks = snacks.sort((a, b) => b.id - a.id)
+const Home: NextPage = () => {  
   const [showModal, toggleModal] = useModal(false)
+  const [isLoading, setLoading] = useState(true)
+  const [snacks, setSnacks] = useState<SnackCard[]>([])
+  const sortedSnacks = snacks.sort((a, b) => b.id - a.id)
+
+  useEffect(() => {
+    getSnacks()
+  }, [])
+
+  async function getSnacks() {
+    try {
+      setLoading(true)
+      const { data, error, status } = await supabase
+        .from('snacks')
+        .select(`id, eventId, title, description, twitterUrl, author, snackUrl`)
+      
+      if (error && status !== 406) {
+        throw error
+      }
+
+      if (data) {
+        setSnacks(data)
+      }
+    } catch (error) {
+      if (typeof error === "string") {
+        alert(error.toUpperCase())
+      } else if (error instanceof Error) {
+        alert(error.message)
+      }
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
     <Container>
@@ -112,20 +70,23 @@ const Home: NextPage = () => {
         </div>
         <div className="flex flex-col w-full">
           <SectionTitle>Snack of the Day</SectionTitle>
-          <SnackCard key={sortedSnacks[0].id} snack={sortedSnacks[0]} />
+          {isLoading ? null : (
+            <SnackCard key={sortedSnacks[0].id} snack={sortedSnacks[0]} />
+          )}
         </div>
         <div className="flex justify-between w-full">
           <SectionTitle>All Snacks</SectionTitle>
           <button onClick={toggleModal}>
             <FontAwesomeIcon icon={faCirclePlus} size="2x" className="flex self-center px-2 pt-2 text-black dark:text-white text-center font-bold" />
           </button>
-          
         </div>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-          {sortedSnacks.map((snack: SnackCard) => (
-            <SnackCard key={snack.id} snack={snack} />
-          ))}
-        </div>
+        {isLoading ? null : (
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            {sortedSnacks.map((snack: SnackCard) => (
+              <SnackCard key={snack.id} snack={snack} />
+            ))}
+          </div>
+        )}
         {showModal ? <Modal toggleModal={toggleModal} /> : null}
       </div>
     </Container>
